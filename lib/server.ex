b012d8ac6e8d6684122a138238e8967e3985ac7f
aaronjenner13@gmail.com <aaronjenner13@gmail.com>
2016-11-10 23:11:58 +0000

    This might work

--- lib/server.ex
+++ lib/server.ex
@@ -3,6 +3,8 @@ defmodule Server do
   import Supervisor.Spec
 
   @port Application.get_env(:server, :port)
+  @max_clients 32
+  @system_ip "89.100.7.32"
 
   def start(_type, _args) do
     IO.puts "Server Started\n"
@@ -13,6 +15,8 @@ defmodule Server do
     options = [strategy: :one_for_one, name: Server.Supervisor]
     Supervisor.start_link(children, options)
   end
+
+  # Listen for connections
 
   def listen(port) do
     IO.puts "Server listening on: #{port}"
@@ -25,33 +29,74 @@ defmodule Server do
         IO.puts "Accepting connections on port #{port}"
         receive_conn(socket)
       _ ->
-        IO.puts "error opening socket connection"
+        IO.puts "ERROR: opening socket connection"
         System.halt
     end
   end
 
+  # Receviving connections below
+
   defp receive_conn(socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
-    {:ok, pid} = Task.Supervisor.start_child(Server.TaskSupervisor, fn -> serve(client) end)
-    :ok = :gen_tcp.controlling_process(client, pid)
-    receive_conn(socket)
+    case :gen_tcp.accept(socket) do
+      {:ok, client} ->
+        serve_client(socket,client)
+       _ ->
+        IO.puts "ERROR: Server socket is closed"
+        System.halt
+    end
+  end
+  
+  # Thread pooling handled below - with workers serving as threads.
+
+  defp serve_client(socket, client_socket) do
+    %{:workers => count} = Supervisor.count_children(Server.TaskSupervisor) #count the number of active workers
+    if count <= @max_clients do
+      case Task.Supervisor.start_child(Server.TaskSupervisor, fn -> serve_request(socket,client_socket) end) do
+        {:ok, pid} ->
+              :gen_tcp.controlling_process(client_socket, pid)
+              socket |> receive_conn
+         _ ->
+          IO.puts "ERROR: spawning new worker"
+          System.halt
+      end
+    else
+      :gen_tcp.close(client_socket)
+      socket |> receive_conn
+    end
   end
 
-  defp read(socket) do
-  	case :gen_tcp.recv(socket, 0) do
-  	  {:ok, data} ->
-  		IO.puts data
-      {:error, reason} ->
-      	IO.puts "Error receiving data: #{reason}"
-  	end
-  	:gen_tcp.close socket
+  defp serve_request(server_socket,client_socket) do
+    case :gen_tcp.recv(client_socket, 0) do
+      { _ , data} ->
+          IO.puts "Request : #{data}"
+          read(server_socket,client_socket,data)
+      _ ->
+          IO.puts "ERROR: Reading from socket"
+    end
   end
 
-  defp send_request(socket, message) do
-  	IO.puts message
-  	get_request_body = "GET /echo.php?message=#{message} HTTP/1.0\r\n\r\n"
-  	send_get(get_request_body, socket)
-    socket
+  #pattern match responses
+
+  defp read( _ ,socket, "HELO" <> " " <> text ) do
+    message = "HELO #{text}IP:#{get_ip_addr}\nPort:#{@port}\nStudentID:12301154\n"
+    :gen_tcp.send(socket,message)
+    :gen_tcp.close(socket)
+  end
+
+  defp read( server_socket , _ , "KILL_SERVICE" <> _ ) do
+    :gen_tcp.close(server_socket)
+  end
+
+  defp read( _ ,socket, _ ) do
+    :gen_tcp.close(socket)
+  end
+
+  defp get_ip_addr() do
+    {:ok, ifs} = :inet.getif()
+    Enum.map(ifs, fn {ip, _broadaddr, _mask} -> ip end)
+    |> hd()
+    |> Tuple.to_list
+    |> Enum.join(".")
   end
 
 end
