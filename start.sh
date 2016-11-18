11108c9d0315bca6f06eefff7734c7693e2532eb
Aaron Jenner <aaronjenner13@gmail.com>
2016-11-18 18:17:18 +0000

    This time i remembered to rename variables :+1:

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ function to_int {
 function do_it {
   if hash mix 2>/dev/null; then
     local port="$1"
-    local -i port_num=$(to_int "${port}" 2>/dev/null)
-    if (( $port_num < 1 || $port_num > 65535 )) ; then
+    local -i portNum=$(to_int "${port}" 2>/dev/null)
+    if (( $portNum < 1 || $portNum > 65535 )) ; then
       echo "${port} is not a valid port" 1>&2
       return
     fi
-    export CS4032_LAB2_PORT=$1
+    export PORT_NUM=$1
     mix run --no-halt
   else
     echo "You don't have the right bits and pieces installed. Read the readme."
