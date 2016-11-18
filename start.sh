81f62efce308c616946515646520fe7e8855810b
Aaron Jenner <aaronjenner13@gmail.com>
2016-11-18 19:19:19 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,13 +8,13 @@ function do_it {
     local port="$1"
     local -i portNum=$(to_int "${port}" 2>/dev/null)
     if (( $portNum < 1 || $portNum > 65535 )) ; then
-      echo "${port} is not a valid port" 1>&2
+      echo "${port} is not a valid port number" 1>&2
       return
     fi
     export PORT_NUM=$1
     mix run --no-halt
   else
-    echo "You don't have the right bits and pieces installed. Read the readme."
+    echo "You need to have elixir and erlang installed at root, see ReadMe for more details."
   fi
 }
 
